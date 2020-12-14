87f9568e088ab108f3a15aae6697757e87e09c35
Narendra <narendra.dubey4@gmail.com>
2020-12-14 17:00:03 +1100

    first commit

---
+++ EnumerateDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace EnumerateDictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var stock = new ConcurrentDictionary<string, int>();
+			stock.TryAdd("kcdc", 0);
+			stock.TryAdd("Puralsight Live 2018", 0);
+			stock.TryAdd("docker", 0);
+
+			foreach (var shirt in stock.ToArray())
+			{
+				stock["kcdc"] += 1;
+				Console.WriteLine(shirt.Key + ": " + shirt.Value);
+			}
+		}
+	}
+}
